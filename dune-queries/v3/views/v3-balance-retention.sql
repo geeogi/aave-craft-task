3ed8419ad1a610fc909a93f90cc56eb49d76fcff
geeogi <george6bennett@gmail.com>
2026-06-10 08:13:06 +0100

    upd

--- dune-queries/v3/views/v3-balance-retention.sql
+++ dune-queries/v3/views/v3-balance-retention.sql
@@ -8,7 +8,7 @@ WITH params AS (
         100 AS min_balance_usd,
         TIMESTAMP '2026-03-30' AS min_cohort_week
 ),
-weekly_balances AS (
+weekly_user_balances AS (
     SELECT
         week,
         user,
@@ -18,92 +18,95 @@ weekly_balances AS (
         week,
         user
 ),
-cohort_source AS (
-    SELECT
-        week,
-        user
-    FROM weekly_balances
-    CROSS JOIN params
-    WHERE current_balance_usd >= params.min_balance_usd
-),
 user_cohorts AS (
     SELECT
         user,
         MIN(week) AS cohort_week
-    FROM cohort_source
+    FROM weekly_user_balances
+    CROSS JOIN params
+    WHERE current_balance_usd >= params.min_balance_usd
     GROUP BY user
 ),
-selected_cohorts AS (
+analysis_cohorts AS (
     SELECT
-        user_cohorts.user,
-        user_cohorts.cohort_week
+        user,
+        cohort_week
     FROM user_cohorts
     CROSS JOIN params
-    WHERE user_cohorts.cohort_week >= params.min_cohort_week
+    WHERE cohort_week >= params.min_cohort_week
 ),
 cohort_sizes AS (
     SELECT
         cohort_week,
         COUNT(*) AS cohort_users
-    FROM selected_cohorts
+    FROM analysis_cohorts
     GROUP BY cohort_week
 ),
 observation_weeks AS (
     SELECT DISTINCT
         week
-    FROM weekly_balances
+    FROM weekly_user_balances
 ),
-cohort_user_weeks AS (
+cohort_week_spine AS (
     SELECT
-        selected_cohorts.cohort_week,
-        selected_cohorts.user,
+        analysis_cohorts.cohort_week,
+        analysis_cohorts.user,
         observation_weeks.week AS observation_week
-    FROM selected_cohorts
+    FROM analysis_cohorts
     INNER JOIN observation_weeks
-        ON observation_weeks.week >= selected_cohorts.cohort_week
+        ON observation_weeks.week >= analysis_cohorts.cohort_week
 ),
-cohort_observations AS (
+cohort_balances_by_week AS (
     SELECT
-        cohort_user_weeks.cohort_week,
-        cohort_user_weeks.observation_week,
-        cohort_user_weeks.user,
-        weekly_balances.current_balance_usd
-    FROM cohort_user_weeks
-    LEFT JOIN weekly_balances
-        ON cohort_user_weeks.user = weekly_balances.user
-       AND cohort_user_weeks.observation_week = weekly_balances.week
+        cohort_week_spine.cohort_week,
+        cohort_week_spine.observation_week,
+        cohort_week_spine.user,
+        weekly_user_balances.current_balance_usd
+    FROM cohort_week_spine
+    LEFT JOIN weekly_user_balances
+        ON cohort_week_spine.user = weekly_user_balances.user
+       AND cohort_week_spine.observation_week = weekly_user_balances.week
+),
+retention_by_week AS (
+    SELECT
+        cohort_balances_by_week.cohort_week,
+        cohort_balances_by_week.observation_week,
+        date_diff('week', cohort_balances_by_week.cohort_week, cohort_balances_by_week.observation_week) AS weeks_since_cohort,
+        cohort_sizes.cohort_users,
+        COUNT_IF(cohort_balances_by_week.current_balance_usd >= params.min_balance_usd) AS retained_users
+    FROM cohort_balances_by_week
+    INNER JOIN cohort_sizes
+        ON cohort_balances_by_week.cohort_week = cohort_sizes.cohort_week
+    CROSS JOIN params
+    GROUP BY
+        cohort_balances_by_week.cohort_week,
+        cohort_balances_by_week.observation_week,
+        date_diff('week', cohort_balances_by_week.cohort_week, cohort_balances_by_week.observation_week),
+        cohort_sizes.cohort_users
 )
 
 SELECT
-    cohort_observations.cohort_week,
-    CAST(cohort_observations.cohort_week AS date) AS cohort_date,
+    retention_by_week.cohort_week,
+    CAST(retention_by_week.cohort_week AS date) AS cohort_date,
     CONCAT(
-        CAST(day_of_month(cohort_observations.cohort_week) AS varchar),
+        CAST(day_of_month(retention_by_week.cohort_week) AS varchar),
         CASE
-            WHEN day_of_month(cohort_observations.cohort_week) IN (11, 12, 13) THEN 'th'
-            WHEN mod(day_of_month(cohort_observations.cohort_week), 10) = 1 THEN 'st'
-            WHEN mod(day_of_month(cohort_observations.cohort_week), 10) = 2 THEN 'nd'
-            WHEN mod(day_of_month(cohort_observations.cohort_week), 10) = 3 THEN 'rd'
+            WHEN day_of_month(retention_by_week.cohort_week) IN (11, 12, 13) THEN 'th'
+            WHEN mod(day_of_month(retention_by_week.cohort_week), 10) = 1 THEN 'st'
+            WHEN mod(day_of_month(retention_by_week.cohort_week), 10) = 2 THEN 'nd'
+            WHEN mod(day_of_month(retention_by_week.cohort_week), 10) = 3 THEN 'rd'
             ELSE 'th'
         END,
         ' ',
-        date_format(cohort_observations.cohort_week, '%b')
+        date_format(retention_by_week.cohort_week, '%b')
     ) AS cohort_label,
-    cohort_observations.observation_week,
-    date_diff('week', cohort_observations.cohort_week, cohort_observations.observation_week) AS weeks_since_cohort,
-    cohort_sizes.cohort_users,
-    COUNT_IF(cohort_observations.current_balance_usd >= params.min_balance_usd) AS retained_users,
-    COUNT_IF(cohort_observations.current_balance_usd >= params.min_balance_usd) * 1.0 / cohort_sizes.cohort_users AS retained_user_share,
-    COUNT_IF(cohort_observations.current_balance_usd >= params.min_balance_usd) * 100.0 / cohort_sizes.cohort_users AS retained_user_pct
-FROM cohort_observations
-INNER JOIN cohort_sizes
-    ON cohort_observations.cohort_week = cohort_sizes.cohort_week
-CROSS JOIN params
-GROUP BY
-    cohort_observations.cohort_week,
-    cohort_observations.observation_week,
-    date_diff('week', cohort_observations.cohort_week, cohort_observations.observation_week),
-    cohort_sizes.cohort_users
+    retention_by_week.observation_week,
+    retention_by_week.weeks_since_cohort,
+    retention_by_week.cohort_users,
+    retention_by_week.retained_users,
+    retention_by_week.retained_users * 1.0 / retention_by_week.cohort_users AS retained_user_share,
+    retention_by_week.retained_users * 100.0 / retention_by_week.cohort_users AS retained_user_pct
+FROM retention_by_week
 ORDER BY
-    cohort_observations.cohort_week,
-    cohort_observations.observation_week
+    retention_by_week.cohort_week,
+    retention_by_week.observation_week
--- dune-queries/v3/views/v3-dollar-retention.sql
+++ dune-queries/v3/views/v3-dollar-retention.sql
@@ -11,7 +11,7 @@ WITH params AS (
         100 AS min_balance_usd,
         TIMESTAMP '2026-03-30' AS min_cohort_week
 ),
-weekly_user_positions AS (
+weekly_positions AS (
     SELECT
         week,
         user,
@@ -23,133 +23,129 @@ weekly_user_positions AS (
         current_balance_usd
     FROM dune.geeogi_team.result_aave_v3_ethereum_user_balances_weekly
 ),
-weekly_balances AS (
+weekly_user_balances AS (
     SELECT
         week,
         user,
         SUM(current_balance_usd) AS current_balance_usd
-    FROM weekly_user_positions
+    FROM weekly_positions
     GROUP BY
         week,
         user
-),
-cohort_source AS (
-    SELECT
-        week,
-        user
-    FROM weekly_balances
-    CROSS JOIN params
-    WHERE current_balance_usd >= params.min_balance_usd
 ),
 user_cohorts AS (
     SELECT
         user,
         MIN(week) AS cohort_week
-    FROM cohort_source
+    FROM weekly_user_balances
+    CROSS JOIN params
+    WHERE current_balance_usd >= params.min_balance_usd
     GROUP BY user
 ),
-selected_cohorts AS (
+analysis_cohorts AS (
     SELECT
-        user_cohorts.user,
-        user_cohorts.cohort_week
+        user,
+        cohort_week
     FROM user_cohorts
     CROSS JOIN params
-    WHERE user_cohorts.cohort_week >= params.min_cohort_week
+    WHERE cohort_week >= params.min_cohort_week
 ),
 cohort_sizes AS (
     SELECT
         cohort_week,
         COUNT(*) AS cohort_users
-    FROM selected_cohorts
+    FROM analysis_cohorts
     GROUP BY cohort_week
 ),
-cohort_positions AS (
+-- Freeze each cohort's starting asset amounts and prices at entry week.
+cohort_entry_positions AS (
     SELECT
-        selected_cohorts.cohort_week,
-        selected_cohorts.user,
-        weekly_user_positions.asset,
-        weekly_user_positions.reserve,
-        weekly_user_positions.a_token,
-        weekly_user_positions.current_a_token_balance AS cohort_amount,
-        weekly_user_positions.asset_price_usd AS cohort_price_usd,
-        weekly_user_positions.current_balance_usd AS cohort_position_usd
-    FROM selected_cohorts
-    INNER JOIN weekly_user_positions
-        ON selected_cohorts.user = weekly_user_positions.user
-       AND selected_cohorts.cohort_week = weekly_user_positions.week
+        analysis_cohorts.cohort_week,
+        analysis_cohorts.user,
+        weekly_positions.asset,
+        weekly_positions.reserve,
+        weekly_positions.a_token,
+        weekly_positions.current_a_token_balance AS cohort_amount,
+        weekly_positions.asset_price_usd AS cohort_price_usd,
+        weekly_positions.current_balance_usd AS cohort_position_usd
+    FROM analysis_cohorts
+    INNER JOIN weekly_positions
+        ON analysis_cohorts.user = weekly_positions.user
+       AND analysis_cohorts.cohort_week = weekly_positions.week
 ),
 cohort_start_values AS (
     SELECT
         cohort_week,
         SUM(cohort_position_usd) AS cohort_start_balance_usd
-    FROM cohort_positions
+    FROM cohort_entry_positions
     GROUP BY cohort_week
 ),
 observation_weeks AS (
     SELECT DISTINCT
         week
-    FROM weekly_user_positions
+    FROM weekly_positions
 ),
-cohort_position_observations AS (
+-- Follow only the cohort-entry positions through later weeks at frozen entry prices.
+cohort_position_values_by_week AS (
     SELECT
-        cohort_positions.cohort_week,
+        cohort_entry_positions.cohort_week,
         observation_weeks.week AS observation_week,
-        cohort_positions.user,
-        cohort_positions.asset,
-        cohort_positions.reserve,
-        cohort_positions.a_token,
-        cohort_positions.cohort_amount,
-        cohort_positions.cohort_price_usd,
-        COALESCE(weekly_user_positions.current_a_token_balance, 0) AS observation_amount
-    FROM cohort_positions
+        cohort_entry_positions.user,
+        cohort_entry_positions.asset,
+        cohort_entry_positions.reserve,
+        cohort_entry_positions.a_token,
+        cohort_entry_positions.cohort_amount,
+        cohort_entry_positions.cohort_price_usd,
+        COALESCE(weekly_positions.current_a_token_balance, 0) AS observation_amount
+    FROM cohort_entry_positions
     INNER JOIN observation_weeks
-        ON observation_weeks.week >= cohort_positions.cohort_week
-    LEFT JOIN weekly_user_positions
-        ON cohort_positions.user = weekly_user_positions.user
-       AND cohort_positions.reserve = weekly_user_positions.reserve
-       AND cohort_positions.a_token = weekly_user_positions.a_token
-       AND observation_weeks.week = weekly_user_positions.week
+        ON observation_weeks.week >= cohort_entry_positions.cohort_week
+    LEFT JOIN weekly_positions
+        ON cohort_entry_positions.user = weekly_positions.user
+       AND cohort_entry_positions.reserve = weekly_positions.reserve
+       AND cohort_entry_positions.a_token = weekly_positions.a_token
+       AND observation_weeks.week = weekly_positions.week
 ),
-cohort_observation_values AS (
+retention_values_by_week AS (
     SELECT
         cohort_week,
         observation_week,
         SUM(observation_amount * cohort_price_usd) AS gross_retained_balance_usd,
         SUM(LEAST(observation_amount, cohort_amount) * cohort_price_usd) AS capped_retained_balance_usd
-    FROM cohort_position_observations
+    FROM cohort_position_values_by_week
     GROUP BY
         cohort_week,
         observation_week
 )
 
 SELECT
-    cohort_observation_values.cohort_week,
-    CAST(cohort_observation_values.cohort_week AS date) AS cohort_date,
+    retention_values_by_week.cohort_week,
+    CAST(retention_values_by_week.cohort_week AS date) AS cohort_date,
     CONCAT(
-        CAST(day_of_month(cohort_observation_values.cohort_week) AS varchar),
+        CAST(day_of_month(retention_values_by_week.cohort_week) AS varchar),
         CASE
-            WHEN day_of_month(cohort_observation_values.cohort_week) IN (11, 12, 13) THEN 'th'
-            WHEN mod(day_of_month(cohort_observation_values.cohort_week), 10) = 1 THEN 'st'
-            WHEN mod(day_of_month(cohort_observation_values.cohort_week), 10) = 2 THEN 'nd'
-            WHEN mod(day_of_month(cohort_observation_values.cohort_week), 10) = 3 THEN 'rd'
+            WHEN day_of_month(retention_values_by_week.cohort_week) IN (11, 12, 13) THEN 'th'
+            WHEN mod(day_of_month(retention_values_by_week.cohort_week), 10) = 1 THEN 'st'
+            WHEN mod(day_of_month(retention_values_by_week.cohort_week), 10) = 2 THEN 'nd'
+            WHEN mod(day_of_month(retention_values_by_week.cohort_week), 10) = 3 THEN 'rd'
             ELSE 'th'
         END,
         ' ',
-        date_format(cohort_observation_values.cohort_week, '%b')
+        date_format(retention_values_by_week.cohort_week, '%b')
     ) AS cohort_label,
-    cohort_observation_values.observation_week,
-    date_diff('week', cohort_observation_values.cohort_week, cohort_observation_values.observation_week) AS weeks_since_cohort,
+    retention_values_by_week.observation_week,
+    date_diff('week', retention_values_by_week.cohort_week, retention_values_by_week.observation_week) AS weeks_since_cohort,
     cohort_sizes.cohort_users,
     cohort_start_values.cohort_start_balance_usd,
-    cohort_observation_values.gross_retained_balance_usd,
-    cohort_observation_values.gross_retained_balance_usd * 100.0 / cohort_start_values.cohort_start_balance_usd AS gross_retained_balance_pct,
-    cohort_observation_values.capped_retained_balance_usd,
-    cohort_observation_values.capped_retained_balance_usd * 100.0 / cohort_start_values.cohort_start_balance_usd AS capped_retained_balance_pct
-FROM cohort_observation_values
+    retention_values_by_week.gross_retained_balance_usd,
+    retention_values_by_week.gross_retained_balance_usd * 100.0 / cohort_start_values.cohort_start_balance_usd AS gross_retained_balance_pct,
+    retention_values_by_week.capped_retained_balance_usd,
+    retention_values_by_week.capped_retained_balance_usd * 100.0 / cohort_start_values.cohort_start_balance_usd AS capped_retained_balance_pct
+FROM retention_values_by_week
 INNER JOIN cohort_sizes
-    ON cohort_observation_values.cohort_week = cohort_sizes.cohort_week
+    ON retention_values_by_week.cohort_week = cohort_sizes.cohort_week
 INNER JOIN cohort_start_values
-    ON cohort_observation_values.cohort_week = cohort_start_values.cohort_week
+    ON retention_values_by_week.cohort_week = cohort_start_values.cohort_week
 ORDER BY
-    cohort_observation_values.cohort_week,
-    cohort_observation_values.observation_week
+    retention_values_by_week.cohort_week,
+    retention_values_by_week.observation_week
